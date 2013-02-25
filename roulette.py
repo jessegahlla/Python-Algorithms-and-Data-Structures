eee1768f69f077f4a2aef1931175000a831e6bc7
Jesse Singh <jesse.singh@gmail.com>
2013-02-24 19:18:17 -0800

    Roulette Game

--- roulette.py
+++ roulette.py
@@ -1,51 +1,70 @@
 import random
-
-random.seed(12345)
+import matplotlib.pyplot as plt
+import numpy as np
 
 def spinwheel():
 	return(random.randrange(0,36,1))
 
 def roulette(bet = None, betOn = None):
 	landOn = spinwheel()
-	print("Landed on: %s" % landOn)
 	if landOn == 0:
 		return 0
 	elif landOn % 2 == 0:
 		if betOn == "e":
-			print("\tYou bet even, you won!")
 			return 2*bet
 		else:
-			print("\tYou lost.")
 			return 0
 	elif landOn % 2 == 1:
 		if betOn == "e":
-			print("\tYou lost.")
 			return 0
 		else:
-			print("\tYou bet odd, you win!")
 			return 2*bet
 	else:
 		print "Huh?"
 
-def playMartingale(wallet = None, minBet = 0.25, maxBet = 90, hardStop = 10):
+def playMartingale(wallet = None, minBet = 0.25, maxBet = 90, hardStop = 100):
 	tries = 0
 	totalPot = []
 	totalPot.append(wallet)
 	winnings = 1
 	myBet = minBet
-	while wallet > minBet:
+	while wallet > minBet and tries < hardStop:
 		if winnings == 0:
 			myBet = min(myBet*2, maxBet)
-			if myBet == maxBet:
-				print("MAX BET")
 		else:
 			myBet = minBet
 		wallet = wallet - myBet
-		print("Betting %s" % myBet)
 		winnings = roulette(myBet, betOn = "e")
 		wallet = wallet + winnings
 		totalPot.append(wallet)
-		print("Wallet is now %f" % wallet)
 		tries = tries + 1
-	print("Broke at %s tries" % tries)
 	return totalPot
+
+def visitsToCasino(n = 10, seedInt=12345):
+	visit = {}
+	netGain = {}
+	random.seed(seedInt)
+	for i in range(n):
+		print("Visit %s out of %s" % (i, n))
+		visit[i] = playMartingale(wallet = 127.75)
+		netGain[i] = np.array(visit[i]) - 127.75
+	return visit, netGain
+
+def plotWallet(plotMe = []):
+	fig = plt.figure()
+	ax = fig.add_subplot(111)
+	ax.plot(plotMe)
+	plt.xlabel("Rounds")
+	plt.ylabel("Net Gain ($)")
+	plt.title("The Martingale Betting System")
+	plt.show()
+
+def histWallet(plotMe = [], numBins = 100):
+	fig = plt.figure()
+	ax = fig.add_subplot(111)
+	n, bins, patches = plt.hist(plotMe, numBins, normed=True)
+	plt.setp(patches, "facecolor", "g", linewidth=0.0)
+	plt.xlabel("Net Gain ($)")
+	plt.ylabel("Frequency")
+	plt.title("Size of Pot")
+	plt.show()
